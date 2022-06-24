5b99e1545ad0d57e26936128068188f27dacdf31
andytxr <andreluiiz.gst@gmail.com>
2022-06-24 18:11:15 -0300

    Preparing the homepage to search questions

---
+++ app/controllers/site/search_controller.rb
@@ -0,0 +1,9 @@
+class Site::SearchController < SiteController
+
+    def questions
+        
+        @questions = Question.includes(:answers).page(params[:page])
+
+    end
+
+end
--- app/controllers/site/welcome_controller.rb
+++ app/controllers/site/welcome_controller.rb
@@ -1,4 +1,7 @@
 class Site::WelcomeController < SiteController
+
   def index
+    @questions = Question.includes(:answers).page(params)
   end
+
 end
