00a9a6fd353f77c08ccba62838740de33cf72fed
andytxr <andreluiiz.gst@gmail.com>
2022-07-04 17:26:47 -0300

    Implementing Elasticsearch

--- app/controllers/site/search_controller.rb
+++ app/controllers/site/search_controller.rb
@@ -2,7 +2,7 @@ class Site::SearchController < SiteController
 
     def questions
         
-       @questions = Question._search_(params[:page], params[:term])
+       @questions = Question.search(params[:term], page: params[:page], per_page: 5)
 
     end
 
